44baf55075859756fdeb2d7854ce6365929cc7a7
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-01 19:41:13 +0530

    v2.0.5

--- subscription.php
+++ subscription.php
@@ -1,5 +1,8 @@
 <?php
+session_start();
 include( "_loader.php" );
+$row_config_globale = $cnx->SqlRow( "SELECT * FROM $table_global_config" );
+include( "include/lang/" . $row_config_globale[ 'language' ] . ".php" );
 $list_id    = ( !empty( $_POST[ 'list_id' ] ) ? $_POST[ 'list_id' ] : "" );
 $list_id    = ( empty( $list_id ) && !empty( $_GET[ 'list_id' ] ) ? $_GET[ 'list_id' ] : $list_id );
 $email_addr = ( !empty( $_POST[ 'email_addr' ] ) ? $_POST[ 'email_addr' ] : "" );
@@ -22,35 +25,27 @@ if ( $op == "leave" && !$row_config_globale[ 'unsub_validation' ] ) {
 	$op = "join";
 }
 $news = getConfig( $cnx, $list_id, $row_config_globale[ 'table_listsconfig' ] );
+require( 'include/lib/PHPMailerAutoload.php' );
 $tPath        = ($row_config_globale['path'] == '' ? '/' : '/'.$row_config_globale['path']);
 $tPath        = str_replace('//','/',$tPath);
-use PHPMailer;
-use Exception;
-require 'include/lib/PHPMailer/src/Exception.php';
-require 'include/lib/PHPMailer/src/PHPMailer.php';
-require 'include/lib/PHPMailer/src/SMTP.php';
-if ( $do_encrypt==1 ){
-	include('include/lib/class.encrypt.php');
-	$en = new Encrypt();
-	$email_sub = $en->decrypt($email_addr);
-	$email_addr = $email_sub;
-} else {
-	$email_sub = $email_addr;
-}
-echo '<!DOCTYPE HTML>
-<html lang="' . tr( "LN" ) . '">
+?>
+<!DOCTYPE HTML>
+<html lang="<?php echo tr( "LN" ); ?>">
 	<head>
 		<meta charset="utf-8" />
-		<title>' . tr( "NEWSLETTER_TITLE" ) . '</title>
+		<title><?php  echo tr( "NEWSLETTER_TITLE" ); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script src="js/wysiwyg/jquery-1.10.2.min.js"></script>
 		<script src="js/wysiwyg/jquery-ui.js"></script>
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<link href="//code.jquery.com/ui/1.12.0/themes/redmond/jquery-ui.css" rel="stylesheet" media="screen">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" rel="stylesheet">
+		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
-		<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/i18n/defaults-' . tr("I18N_LNG") . '.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+		<!-- (Optional) Latest compiled and minified JavaScript translation files -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/i18n/defaults-<?php echo tr("I18N_LNG");?>.min.js"></script>
 		<!--[if lt IE 9]>
 			<script src="//oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
 			<script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -58,11 +53,12 @@ echo '<!DOCTYPE HTML>
 	</head>
 	<body>
 		<div class="container-fluid">
-			<div class="col-md-12">';
+			<div class="col-md-12">
+			<?php 
 			if ( isset( $list_id ) && !empty( $list_id ) && isValidNewsletter( $cnx, $row_config_globale[ 'table_listsconfig' ], $list_id ) && isset( $email_addr ) ) {
 				if ( !validEmailAddress( $email_addr ) ) {
-					echo '<header><h4>' . tr( "SUBSCRIPTION_TITLE" ) . '</h4></header>
-						<h4 class="alert alert-danger">' . tr( "EMAIL_ADDRESS_NOT_VALID" ) . '</div>';
+					echo '<header><h4>' . tr( "SUBSCRIPTION_TITLE" ) . '</h4></header>';
+					echo "<h4 class='alert alert-danger'>" . tr( "EMAIL_ADDRESS_NOT_VALID" ) . "</div>";
 					exit( );
 				}
 				switch ( $op ) {
@@ -72,10 +68,7 @@ echo '<!DOCTYPE HTML>
 						if ( empty( $c ) || ( $_POST[ 'c' ] != $_SESSION[ 'c' ] ) ) {
 							$_SESSION[ 'new_sub' ] = $email_addr;
 							echo '<form method="post" action="">
-								<div class="row">
-									<div class="col-md-12">' 
-										. tr( "SUBSCRIPTION_CAPTCHA" ) . '
-									</div>
+								<div class="row">' . tr( "SUBSCRIPTION_CAPTCHA" ) . '
 									<div class="col-md-6">
 										<img src="c.php" />
 									</div>
@@ -96,20 +89,20 @@ echo '<!DOCTYPE HTML>
 							if ( $row_config_globale[ 'mod_sub' ] == "0" ) {
 								$add = addSubscriberTemp( $cnx, $row_config_globale[ 'table_email' ], $row_config_globale[ 'table_temp' ], $list_id, $email_addr );
 								if ( strlen( $add ) > 3 ) {
-									$body = $news[ 'subscription_body' ]
-										. "\n\n" . tr( "SUBSCRIPTION_MAIL_BODY" ) . ":\n
-										<a href='" . $row_config_globale[ 'base_url' ] . $tPath . "subscription.php?op=confirm_join&email_addr=" . urlencode( $email_addr ) 
+									$body = $news[ 'subscription_body' ];
+									$body .= "\n\n" . tr( "SUBSCRIPTION_MAIL_BODY" ) . ":\n";
+									$body .= "<a href='" . $row_config_globale[ 'base_url' ] . $tPath . "subscription.php?op=confirm_join&email_addr=" . urlencode( $email_addr ) 
 										. "&hash=$add&list_id=$list_id'>" . tr( "SUBSCRIPTION_I_SUB" ) . "</a>";
 									$subj = ( strtoupper( $row_config_globale[ 'charset' ] ) == "UTF-8" 
 										? $news[ 'subscription_subject' ] 
 										: iconv( "UTF-8", $row_config_globale[ 'charset' ], $news[ 'subscription_subject' ] ) );
 									$body = ( strtoupper( $row_config_globale[ 'charset' ] ) == "UTF-8" ? $body : iconv( "UTF-8", $row_config_globale[ 'charset' ], $body ) );
 									if ( $row_config_globale[ 'sending_method' ] == 'lbsmtp' ) {
-										$info_smtp_lb = $cnx->query( "SELECT * 
+										$info_smtp_lb = $cnx->SqlRow( "SELECT * 
 											FROM " . $row_config_globale[ 'table_smtp' ] . " 
 												WHERE smtp_used < smtp_limite
 													AND smtp_date_update > DATE_SUB(CURDATE(), INTERVAL 1 DAY)
-											ORDER BY id_use ASC LIMIT 1" )->fetch();
+											ORDER BY id_use ASC LIMIT 1" );
 										if ( $info_smtp_lb[ 'smtp_user' ] != '' && $info_smtp_lb[ 'smtp_pass' ] != '' ) {
 											$auth = 1;
 										} else {
@@ -192,7 +185,7 @@ echo '<!DOCTYPE HTML>
 								 $row_config_globale[ 'smtp_auth' ], $row_config_globale[ 'smtp_host' ], $row_config_globale[ 'smtp_login' ], 
 								 $row_config_globale[ 'smtp_pass' ], $row_config_globale[ 'charset' ] );
 							echo "<h4 class='alert alert-success'>" . tr( "SUBSCRIPTION_FINISHED" ) . "</h4>";
-							if ( $row_config_globale[ 'alert_sub' ] == '1' ) {
+							if ( $row_config_globale[ 'alert_sub' ] == 1 ) {
 								$rapport_sujet = tr( "SUBSCRIPTION_TITLE" );
 								$subj          = ( strtoupper( $row_config_globale[ 'charset' ] ) == "UTF-8" ? $rapport_sujet 
 									: iconv( "UTF-8", $row_config_globale[ 'charset' ], $rapport_sujet ) );
@@ -222,9 +215,9 @@ echo '<!DOCTYPE HTML>
 						break;
 					case "confirm_leave":
 						echo '<header><h4>' . tr( "UNSUBSCRIPTION_TITLE" ) . '</h4></header>';
-						$rm = removeSubscriber( $cnx, $row_config_globale[ 'table_email' ], $row_config_globale[ 'table_send' ],
-							$list_id, $email_addr, $hash, $i, $row_config_globale[ 'table_email_deleted' ] );
-						if ( $alert_unsub == '1' && $rm ) {
+						$rm = removeSubscriber( $cnx, $row_config_globale[ 'table_email' ], $row_config_globale[ 'table_send' ], $list_id, $email_addr, 
+							$hash, $i, $row_config_globale[ 'table_email_deleted' ] );
+						if ( !$row_config_globale[ 'unsub_validation' ] ) {
 							sendEmail( $row_config_globale[ 'sending_method' ], $news[ 'from_addr' ], $news[ 'from_addr' ], $news[ 'from_name' ], 'Désinscription', 
 								'Liste : ' . $list_id . '<br />Désinscrit : ' . $email_addr, $row_config_globale[ 'smtp_auth' ], $row_config_globale[ 'smtp_host' ], 
 								$row_config_globale[ 'smtp_login' ], $row_config_globale[ 'smtp_pass' ], $row_config_globale[ 'charset' ] );
@@ -260,7 +253,7 @@ echo '<!DOCTYPE HTML>
 								sendEmail( $row_config_globale[ 'sending_method' ], $email_addr, $news[ 'from_addr' ], $news[ 'from_name' ], 
 									$subj, $body, $row_config_globale[ 'smtp_auth' ], $row_config_globale[ 'smtp_host' ], $row_config_globale[ 'smtp_login' ],
 									$row_config_globale[ 'smtp_pass' ], $row_config_globale[ 'charset' ] );
-								if ( $row_config_globale[ 'alert_sub' ] == '1'   ) {
+								if ( $row_config_globale[ 'alert_sub' ] == 1 ) {
 									$rapport_sujet = tr( "SUBSCRIPTION_TITLE" );
 									$subj          = ( strtoupper( $row_config_globale[ 'charset' ] ) == "UTF-8" ? $rapport_sujet 
 										: iconv( "UTF-8", $row_config_globale[ 'charset' ], $rapport_sujet ) );
@@ -292,47 +285,29 @@ echo '<!DOCTYPE HTML>
 							}
 						}
 						echo '<h4 class="alert alert-info">' . tr( "CLOSE_WINDOW" ) . '</h4>';
+						
 						break;
 					case "leave_direct":
 						echo '<header><h4>' . tr( "UNSUBSCRIPTION_TITLE" ) . '</h4></header>';
 						if ( !$row_config_globale[ 'unsub_validation' ] ) {
 							$rm = removeSubscriberDirect( $cnx, $row_config_globale[ 'table_email' ], $row_config_globale[ 'table_send' ], $list_id, $email_addr, 
 								$h, $i, $row_config_globale[ 'table_email_deleted' ] );
-							if ( $rm==true ) {
+							sendEmail( $row_config_globale[ 'sending_method' ], $news[ 'from_addr' ], $news[ 'from_addr' ], 
+								$news[ 'from_name' ], 'Désinscription', 'Liste : ' . $list_id . '<br />Désinscrit : ' 
+								. $email_addr, $row_config_globale[ 'smtp_auth' ], 
+								$row_config_globale[ 'smtp_host' ], $row_config_globale[ 'smtp_login' ], 
+								$row_config_globale[ 'smtp_pass' ], $row_config_globale[ 'charset' ] );
+							if ( $rm ) {
 								echo "<h4 class='alert alert-success'>" . tr( "UNSUBSCRIPTION_FINISHED" ) . ".</h4>";
-							} else if ( $rm ==false ) {
+							} else if ( $rm == -1 ) {
 								echo "<h4 class='alert alert-danger'>" . tr( "UNSUBSCRIPTION_UNKNOWN_EMAIL_ADDRESS" ) . "</h4>";
 							} else {
 								echo "<h4 class='alert alert-danger'>" . tr( "ERROR_UNKNOWN" ) . "</h4>";
 							}
 						}
-						if ( $alert_unsub == '1' && $rm ) {
-							$rapport_sujet = tr( "UNSUBSCRIPTION_TITLE" );
-							$subj          = ( strtoupper( $row_config_globale[ 'charset' ] ) == "UTF-8" ? $rapport_sujet 
-								: iconv( "UTF-8", $row_config_globale[ 'charset' ], $rapport_sujet ) );
-							$rapport       = '<br /><br /><br /><br /><br />
-								<table style="height: 217px; margin-left: auto; margin-right: auto;" width="660">
-								<tbody>
-								<tr><td style="text-align: center;" colspan="2"><span style="color: #2446a2;font-size: 14pt;">
-									<img src="https://www.phpmynewsletter.com/css/images/phpmynewsletter_v2.png" 
-									alt="" width="123" height="72" /><br />' 
-									. tr( "UNSUBSCRIPTION_TITLE" ) . ' !</td></tr>
-								<tr><td><span style="color: #2446a2;">' . tr( "LIST_NUMBER" ) . ' :</span></td>
-									<td><span style="color: #2446a2;">' . $list_id . '</span></td></tr>
-								<tr><td><span style="color: #2446a2;">' . tr( "EMAIL_ADDRESS" ) . ' :</span></td>
-									<td><span style="color: #2446a2;">' . $email_addr . '</td></tr>
-								</tbody>
-								</table>';
-							sendEmail( $row_config_globale[ 'sending_method' ], $row_config_globale[ 'admin_email' ], 
-								$row_config_globale[ 'admin_email' ], $row_config_globale[ 'admin_name' ], 
-								$subj, $rapport, $row_config_globale[ 'smtp_auth' ], $row_config_globale[ 'smtp_host' ], 
-								$row_config_globale[ 'smtp_login' ], $row_config_globale[ 'smtp_pass' ], 
-								$row_config_globale[ 'charset' ] );
-						}
-						if ( @$unsub_validation_sms == 1 ) {
+						if ( $unsub_validation_sms == 1 )
 							send_sms( $free_id, $free_pass, "Une désinscription sur la liste " . $list_id 
 							. " : " . $email_addr . ". Bonne journée" );
-						}
 						echo '<h4 class="alert alert-info">' . tr( "CLOSE_WINDOW" ) . '</h4>';
 						break;
 					default:
@@ -340,7 +315,8 @@ echo '<!DOCTYPE HTML>
 						break;
 				}
 			}
-echo '			</div>
+			?>
+			</div>
 		</div>
 	</body>
-</html>';
+</html>
